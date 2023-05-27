36b5e1856cec694c3d29fcdda172590b4019fd19
OtakuZhang <zhangyk@shanghaitech.edu.cn>
2023-05-27 17:36:25 -0400

    modified me

---
+++ Assets/Game/Rooms/Operation_Room/RoomOperation_Room.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using PowerTools.Quest;
+using PowerScript;
+using static GlobalScript;
+
+public class RoomOperation_Room : RoomScript<RoomOperation_Room>
+{
+
+
+	IEnumerator OnInteractPropDoor( IProp prop )
+	{
+
+		yield return E.Break;
+	}
+}
