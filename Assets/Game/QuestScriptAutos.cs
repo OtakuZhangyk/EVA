baed7f2ac20b0ec6cec5ebcb8ff1859eceec68c6
OtakuZhang <zhangyk@shanghaitech.edu.cn>
2023-05-27 17:25:00 -0400

    added operation room

--- Assets/Game/QuestScriptAutos.cs
+++ Assets/Game/QuestScriptAutos.cs
@@ -46,6 +46,7 @@ namespace PowerScript
 		// Access to specific room (Auto-generated)
 		public static IRoom Title          { get { return PowerQuest.Get.GetRoom("Title"); } }
 		public static IRoom Forest         { get { return PowerQuest.Get.GetRoom("Forest"); } }
+		public static IRoom Operation_Room { get { return PowerQuest.Get.GetRoom("Operation_Room"); } }
 		// #ROOM# - Do not edit this line, it's used by the system to insert rooms for easy access
 	}
 
