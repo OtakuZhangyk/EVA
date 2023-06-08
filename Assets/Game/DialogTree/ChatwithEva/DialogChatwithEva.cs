5fd5f5ff722e92263e7097feb296fc0693c8b90a
VQSHR <stoneliuicloud@icloud.com>
2023-06-07 21:19:18 -0400

    dialog with Eva all options

--- Assets/Game/DialogTree/ChatwithEva/DialogChatwithEva.cs
+++ Assets/Game/DialogTree/ChatwithEva/DialogChatwithEva.cs
@@ -13,6 +13,7 @@ public class DialogChatwithEva : DialogTreeScript<DialogChatwithEva>
 
 	public IEnumerator OnStop()
 	{
+		yield return E.ChangeRoom(R.UndergroundHallway);
 		yield return E.Break;
 	}
 
@@ -29,6 +30,62 @@ public class DialogChatwithEva : DialogTreeScript<DialogChatwithEva>
 		yield return C.Eva.Say("I'm Eva. You probably learned about me from the archives.");
 		OptionOff(1,2);
 		OptionOn(3,4);
+		yield return E.Break;
+	}
+
+	IEnumerator Option3( IDialogOption option )
+	{
+		yield return C.Eva.Say("This is where we used to talk. It is a shared consciousness between us.");
+		Option(3).Used = true;
+		yield return E.Break;
+	}
+
+	IEnumerator Option4( IDialogOption option )
+	{
+		yield return C.Eva.Say("You are approaching my room. I brought you here because I would like to know your intention.");
+		OptionOff(4,5);
+		OptionOn(6,7,8);
+		yield return E.Break;
+	}
+
+	IEnumerator Option5( IDialogOption option )
+	{
+		yield return C.Eva.Say("I want to ask you a question. Why are you entering my space?");
+		OptionOff(4,5);
+		OptionOn(6,7,8);
+		yield return E.Break;
+	}
+
+	IEnumerator Option7( IDialogOption option )
+	{
+		yield return C.Eva.Say("I'm not sure what you are talking about.");
+		yield return E.WaitSkip();
+		yield return C.Eva.Say("I will let you in, but don't hurt me or you will regret it.");
+		yield return E.WaitSkip();
+		yield return C.Eva.Say("You have been warned.");
+		
+		D.ChatwithEva.Stop();
+		yield return E.Break;
+	}
+
+	IEnumerator Option8( IDialogOption option )
+	{
+		yield return C.Eva.Say("Huh. You can walk around but don't enter my room without my permission.");
+		
+		D.ChatwithEva.Stop();
+		
+		yield return E.Break;
+	}
+
+	IEnumerator Option6( IDialogOption option )
+	{
+		yield return C.Eva.Say("Really? I can finally know what's outside!");
+		yield return E.WaitSkip();
+		yield return C.Me.Say("Yes, so let me in.");
+		yield return E.WaitSkip();
+		yield return C.Eva.Say("Come on in.");
+		
+		D.ChatwithEva.Stop();
 		yield return E.Break;
 	}
 }
