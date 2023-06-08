22b6db71082063d267b8bbbed7a280da2f43d9ff
VQSHR <stoneliuicloud@icloud.com>
2023-06-08 00:04:57 -0400

    lab interactions

--- Assets/Game/Rooms/Lab/RoomLab.cs
+++ Assets/Game/Rooms/Lab/RoomLab.cs
@@ -6,8 +6,8 @@ using static GlobalScript;
 
 public class RoomLab : RoomScript<RoomLab>
 {
-
-
+	
+	
 	IEnumerator OnInteractPropUndergroundHallway( IProp prop )
 	{
 		yield return C.WalkToClicked();
@@ -40,7 +40,10 @@ public class RoomLab : RoomScript<RoomLab>
 
 	IEnumerator OnInteractPropEVA( IProp prop )
 	{
-
+		yield return E.FadeOut();
+		yield return C.Display("Free me.");
+		yield return E.WaitSkip();
+		yield return E.FadeIn();
 		yield return E.Break;
 	}
 
@@ -50,12 +53,18 @@ public class RoomLab : RoomScript<RoomLab>
 		yield return E.WaitSkip();
 		prop.Disable();
 		I.HeadGear.Add();
+		
+		yield return E.FadeOut();
+		yield return C.Display("Put it on.");
+		yield return E.WaitSkip();
+		yield return E.FadeIn();
 		yield return E.Break;
 	}
 
 	IEnumerator OnLookAtPropElectricHelm( IProp prop )
 	{
-
+		yield return C.FaceClicked();
+		yield return C.Me.Say(" That must the amplifier head gear.");
 		yield return E.Break;
 	}
 
@@ -64,5 +73,60 @@ public class RoomLab : RoomScript<RoomLab>
 		yield return C.FaceClicked();
 		yield return E.WaitSkip();
 		yield return C.Me.Say("Door to the hallway");
+	}
+
+	IEnumerator OnUseInvPropInjectionDevice( IProp prop, IInventory item )
+	{
+		
+		yield return E.Break;
+	}
+
+	IEnumerator OnLookAtPropLabMonitor( IProp prop )
+	{
+		yield return C.WalkToClicked();
+		yield return C.FaceClicked();
+		yield return E.WaitSkip();
+		yield return C.Me.Say("Eva's vital signs are stable.");
+		yield return E.ConsumeEvent;
+	}
+
+	IEnumerator OnInteractPropLabMonitor( IProp prop )
+	{
+		yield return E.Break;
+	}
+
+	IEnumerator OnInteractPropInjectionDevice( IProp prop )
+	{
+		yield return C.WalkToClicked();
+		yield return E.WaitSkip();
+		Audio.Play("drawer_open");
+		yield return C.Me.Say("It only accept injectors.");
+		Audio.Play("drawer_close");
+		yield return E.Break;
+	}
+
+	IEnumerator OnLookAtPropInjectionDevice( IProp prop )
+	{
+		yield return C.WalkToClicked();
+		yield return C.FaceClicked();
+		yield return E.WaitSkip();
+		yield return C.Me.Say("It has injector-shaped trays.");
+		yield return E.Break;
+	}
+
+	IEnumerator OnLookAtPropLaptop( IProp prop )
+	{
+		yield return C.FaceClicked();
+		yield return E.WaitSkip();
+		yield return C.Me.Say("Looks like it's being used as a logging device.");
+		yield return E.ConsumeEvent;
+	}
+
+	IEnumerator OnInteractPropLaptop( IProp prop )
+	{
+		yield return C.WalkToClicked();
+		yield return C.FaceClicked();
+		yield return E.WaitSkip();
+		yield return C.Me.Say("No abonormal behaviors logged. Seems normal.");
 	}
 }
